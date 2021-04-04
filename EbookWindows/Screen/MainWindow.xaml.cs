0217a677571ce1ede4f028997622e5cde8c98dad
Rhea <vnofcrhea@gmail.com>
2021-04-04 16:53:58 +0700

    Add project files.

---
+++ EbookWindows/Screen/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EbookWindows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        private TimeSpan SpanTime;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!dispatcherTimer.IsEnabled)
+            {
+                dispatcherTimer.IsEnabled = true;
+                return;
+            }
+            BottomPanelTool.Visibility = Visibility.Visible;
+            TopPanelTool.Visibility = Visibility.Visible;
+            if (dispatcherTimer.IsEnabled)
+            {
+                dispatcherTimer.Interval = new TimeSpan(0, 0, 2); ;
+                dispatcherTimer.Start();
+                dispatcherTimer.Tick += new EventHandler(OnTimedEvent);
+            }
+        }
+        private void OnTimedEvent(object source, EventArgs e)
+        {
+            dispatcherTimer.Stop();
+            SpanTime = new TimeSpan();
+            SpanTime = SpanTime.Add(dispatcherTimer.Interval);
+            this.Dispatcher.Invoke(() =>
+            {
+                BottomPanelTool.Visibility = Visibility.Hidden;
+                TopPanelTool.Visibility = Visibility.Hidden;
+            });
+        }
+
+        private void ShowHideToolButton_Click(object sender, RoutedEventArgs e)
+        {
+            BottomPanelTool.Visibility = Visibility.Collapsed;
+            TopPanelTool.Visibility = Visibility.Collapsed;
+            dispatcherTimer.Stop();
+            var data = ((sender as Button).Content as MaterialDesignThemes.Wpf.PackIcon);
+            if (data.Kind.ToString().Equals("Hide"))
+            {
+                ((sender as Button).Content as MaterialDesignThemes.Wpf.PackIcon).Kind = MaterialDesignThemes.Wpf.PackIconKind.Eye;
+                BottomPanelTool.Visibility = Visibility.Visible;
+                TopPanelTool.Visibility = Visibility.Visible;
+                MainGrid.MouseMove += new MouseEventHandler(StackPanel_MouseMove);
+            }
+            else
+            {
+                ((sender as Button).Content as MaterialDesignThemes.Wpf.PackIcon).Kind = MaterialDesignThemes.Wpf.PackIconKind.EyeOff;
+                BottomPanelTool.Visibility = Visibility.Collapsed;
+                TopPanelTool.Visibility = Visibility.Collapsed;
+                MainGrid.MouseMove -= new MouseEventHandler(StackPanel_MouseMove);
+            }
+        }
+
+        private void OnCopy(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EbookWindows/Screen/WindowScreen.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EbookWindows.Screen
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    /// }
+
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public int Completion { get; set; }
+    }
+    public partial class WindowScreen : Window
+    {
+        public WindowScreen()
+        {
+            InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight; List<TodoItem> items = new List<TodoItem>();
+            items.Add(new TodoItem() { Title = "Complete this WPF tutorial", Completion = 45 });
+            items.Add(new TodoItem() { Title = "Learn C#", Completion = 80 });
+            items.Add(new TodoItem() { Title = "Wash the car", Completion = 0 });
+
+            lbTodoList.ItemsSource = items;
+        }
+
+        private void DragStart(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (WindowState != WindowState.Normal)
+                {
+                    double percentHorizontal = e.GetPosition(this).X / ActualWidth;
+                    double targetHorizontal = RestoreBounds.Width * percentHorizontal;
+
+                    double percentVertical = e.GetPosition(this).Y / ActualHeight;
+                    double targetVertical = RestoreBounds.Height * percentVertical;
+
+                    var lMousePosition = e.GetPosition(this);
+
+                    Left = lMousePosition.X - targetHorizontal;
+                    Top = lMousePosition.Y - targetVertical;
+                    WindowState = WindowState.Normal;
+                }
+                this.DragMove();
+            }
+        }
+        private void Maximize(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else
+                this.WindowState = WindowState.Maximized;
+        }
+        private void Minimize(object sender, RoutedEventArgs e)
+        {
+
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void DragStart1(object sender, QueryContinueDragEventArgs e)
+        {
+
+        }
+
+
+    }
+}
