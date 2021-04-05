dbf2f0099fda6ae2fb67fdaf2068f547c22c637b
NHẬT TRẦN MINH <1712635@student.hcmus.edu.vn>
2021-04-05 23:28:16 +0700

    Add Recentbook ant fix Resize in Multi-Monitor

--- EbookWindows/Screen/RecentBook.xaml.cs
+++ EbookWindows/Screen/RecentBook.xaml.cs
@@ -26,8 +26,23 @@ namespace EbookWindows.Screen
             items.Add(new TodoItem() { Title = "Complete this WPF tutorial", Completion = 45 });
             items.Add(new TodoItem() { Title = "Learn C#", Completion = 80 });
             items.Add(new TodoItem() { Title = "Wash the car", Completion = 0 });
-
+            items.Add(new TodoItem() { Title = "Wash the car", Completion = 0 });
+            items.Add(new TodoItem() { Title = "Wash the car", Completion = 0 });
+            items.Add(new TodoItem() { Title = "Wash the car", Completion = 0 });
+            items.Add(new TodoItem() { Title = "Wash the car", Completion = 0 });
+            items.Add(new TodoItem() { Title = "Wash the car", Completion = 0 });
+            items.Add(new TodoItem() { Title = "Wash the car", Completion = 0 });
+            items.Add(new TodoItem() { Title = "Wash the car", Completion = 0 });
             lbTodoList.ItemsSource = items;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ScrollList.ScrollToHorizontalOffset(ScrollList.HorizontalOffset + 150);
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            ScrollList.ScrollToHorizontalOffset(ScrollList.HorizontalOffset - 150);
         }
     }
 }
--- EbookWindows/Screen/WindowScreen.xaml.cs
+++ EbookWindows/Screen/WindowScreen.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -37,23 +38,44 @@ namespace EbookWindows.Screen
 
             lbTodoList.ItemsSource = items;
         }
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
 
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static Point GetMousePosition()
+        {
+            var w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
         private void DragStart(object sender, MouseEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 if (WindowState != WindowState.Normal)
-                {
+                {   
                     double percentHorizontal = e.GetPosition(this).X / ActualWidth;
                     double targetHorizontal = RestoreBounds.Width * percentHorizontal;
 
                     double percentVertical = e.GetPosition(this).Y / ActualHeight;
                     double targetVertical = RestoreBounds.Height * percentVertical;
 
-                    var lMousePosition = e.GetPosition(this);
-
-                    Left = lMousePosition.X - targetHorizontal;
-                    Top = lMousePosition.Y - targetVertical;
+                    var lMousePosition = GetMousePosition();
+                    if (Left<0)
+                        Left = lMousePosition.X - targetHorizontal;
+                    else
+                        Left = lMousePosition.X - targetHorizontal;
+                    if (Top < 0)
+                        Top = lMousePosition.Y - targetVertical;
+                    else
+                        Top = lMousePosition.Y - targetVertical;
                     WindowState = WindowState.Normal;
                 }
                 this.DragMove();
