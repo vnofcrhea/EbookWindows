759e4aedabe8a7f2a4dcc584621d86a24bba7906
NHẬT TRẦN MINH <1712635@student.hcmus.edu.vn>
2021-04-30 19:50:41 +0700

    update changed in old PC

---
+++ EbookWindows/Screen/DetailScreen.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EbookWindows.Screen
+{
+    /// <summary>
+    /// Interaction logic for DetailScreen.xaml
+    /// </summary>
+    public partial class DetailScreen : UserControl
+    {
+        public DetailScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
